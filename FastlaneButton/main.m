9aa2501002159df66e5e1b415169980de4891dcf
Farlei Heinen <farleih@gmail.com>
2017-03-15 10:50:14 +1300

    Initial Commit

---
+++ FastlaneButton/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FastlaneButton
+//
+//  Created by Farlei Heinen on 15/03/17.
+//  Copyright © 2017 FH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
